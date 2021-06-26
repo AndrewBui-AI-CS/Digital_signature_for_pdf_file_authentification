cd0c9b69fbe88c526ad1165429fe466baaec113c
mariorenger <nguyentruonggiang21897@gmail.com>
2021-06-27 00:18:59 +0700

    fix genQR.py

--- genQR.py
+++ genQR.py
@@ -1,10 +1,24 @@
 import qrcode
+from PIL import Image
+from pyzbar import pyzbar
+#sinh qr
+def genQR(data, name):
 
-def genQR():
-	my_url = "test"
-	qr = qrcode.QRCode(version=1, box_size=10, border=5)
-	qr.add_data(my_url)
-	qr.make(fit=True)
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(data)
+    qr.make(fit=True)
 
-	img = qr.make_image(fill='black', back_color='white')
-	img.save('my_codelearn.png')
+    img = qr.make_image(fill='black', back_color='white')
+    img.save(name)
+    
+#doc data tu QR
+def getQRDATA(png):
+    img = Image.open(png)
+    data = pyzbar.decode(img)
+
+    print("qr-code.png decode:" )
+    
+    return data[0][0]
+
+genQR('message', 'testQR.png')
+print(getQRDATA('testQR.png'))
