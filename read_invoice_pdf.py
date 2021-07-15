99af0bd2fafde674284bbd7a4c6d475a9dfa3554
Tung Nguyen <tungn1201@gmail.com>
2021-07-15 21:26:15 +0700

    Thêm trích chọn đặc trưng hóa đơn

--- read_invoice_pdf.py
+++ read_invoice_pdf.py
@@ -1,45 +1,148 @@
+# Import thư viện cần thiết
 import io
+import os.path
 import re
-#import requests
+import requests
 import pdfplumber
+import argparse
 import pandas as pd
-from tabula import read_pdf
-from tabulate import tabulate
+from google_drive_downloader import GoogleDriveDownloader as gdd
 
-# Doc file tren mang
-# def download_file(url):
-#     local_filename = url.split('/')[-1]
+#--------------------------------------------------------------#
+# Thêm đối số dòng lệnh
+"""
+-f/ --file-path : đường dẫn đến file pdf (có thể là url )
+"""
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--file_path", required=True,
+                help="path to pdf file")
+args = vars(ap.parse_args())
 
-#     with requests.get(url) as r:
-#         with open(local_filename, 'wb') as f:
-#             f.write(r.content)
 
-#     return local_filename
+# Đọc file pdf qua link url
+# Nếu link url là link google sharing thì trả về fileID của file pdf
+# Nếu link url dẫn đến file pdf thì trả về tên file
+def download_file(url):
+    download_name = url.split('/')
+    local_filename = download_name[5]
 
-# invoice_url = 'Dia chi file pdf tren mang'
-# inv_pdf = download_file(invoice_url)
+    with requests.get(url) as r:
+        with open(local_filename, 'wb') as f:
+            f.write(r.content)
+    # print(download_name[5])
+    # print(download_name)
+    return local_filename
 
-file_path = 'Hoa_don.pdf'
 
-# with open('Hoa_don_text_ver_1.txt', 'w') as f:
-#     with pdfplumber.open(file_path) as pdf:
-#         page = pdf.pages[0]
-#         text = page.extract_text()
-#         f.write(text)
+file_path = args["file_path"]
 
-with pdfplumber.open(file_path) as pdf:
+
+# Xử lý đường dẫn
+if 'http' in file_path:
+    print('Đây là đường dẫn URL\n-------------')
+    if '.pdf' in file_path:
+        print('Đã có định dạng pdf\n-------------')
+        pdf_file = download_file(file_path)
+    else:
+        print('Chưa có định dạng pdf\n-------------')
+        save_file = download_file(file_path)
+        gdd.download_file_from_google_drive(
+            file_id=save_file, dest_path='./Sample_Hoa_don_mua_hang.pdf')
+        pdf_file = 'Sample_Hoa_don_mua_hang.pdf'
+
+else:
+    print('File có sẵn trên máy offline\n-------------')
+    pdf_file = file_path
+
+# print(pdf_file)
+# print(file_path)
+
+with pdfplumber.open(pdf_file) as pdf:
     page = pdf.pages[0]
     text = page.extract_text()
 
 # print(text)
-# print(type(text))
+tax_idx = []
+address = []
+phone_num = []
 
-# In vao text
-with io.open('Hoa_don_text_ver_1.txt', 'w', encoding="utf-8") as f:
-    f.write(text)
+# Lấy content theo cấu trúc có sẵn
+for row in text.split('\n'):
+    if row.startswith('Đơn vị bán hàng'):
+        company_name = row
+    elif row.startswith('Mã số thuế'):
+        tax_idx.append(row)
+    elif row.startswith('Địa chỉ'):
+        address.append(row)
+    elif row.startswith('Điện thoại'):
+        phone_num.append(row)
+    elif row.startswith('Đơn vị mua hàng'):
+        customer_name = row
+    elif row.startswith('Ngày'):
+        date = row
+    elif row.startswith('Tổng cộng'):
+        price = 'Tổng tiền : ' + row.split(' ')[-1] + ' VND'
+
+company_tax_idx = tax_idx[0]
+company_add = address[0]
+company_tele = phone_num[0]
+
+customer_tax = tax_idx[1]
+customer_add = address[1]
+customer_tele = phone_num[1][:phone_num[1].find('H')]
+
+content = (company_name + '\n' + company_tax_idx + '\n' +
+           company_add + '\n' + company_tele + '\n' + date)
+
+sign_content = (customer_name + ' _ ' + customer_tax + ' _ ' +
+                customer_add + ' _ ' + customer_tele + ' _ ' + price)
+
+# print(content)
+# print('\n+++++++++++++++++++++++')
+# print(sign_content)
+
+# Lưu text file
+# Lưu đặc trưng hóa đơn
+save_path = 'data_for_pay_online_demo/content'
+text_name = 'Hoa_don_' + customer_name.split(':')[-1] + '.txt'
+# print(text_name)
+complete_name = os.path.join(save_path, text_name)
+with open(complete_name, 'w', encoding="utf-8") as f:
+    f.write(content)
 f.close()
 
-# df = read_pdf("Hoa_don.pdf", pages=1)
-# print(tabulate(df))
-# print('----------------')
-# type(tabulate(df))
+# Lưu nội dung để ký
+save_path_02 = 'data_for_pay_online_demo/sign_content'
+sign_name = 'Signature_' + customer_name.split(':')[-1] + '.txt'
+complete_name_02 = os.path.join(save_path_02, sign_name)
+with open(complete_name_02, 'w', encoding="utf-8") as f:
+    f.write(sign_content)
+f.close()
+
+# Lưu tên người viết hóa đơn
+save_path_03 = 'data_for_pay_online_demo/writer'
+writer_name = 'Written_by_' + company_name.split(':')[-1] + '.txt'
+complete_name_03 = os.path.join(save_path_03, writer_name)
+with open(complete_name_03, 'w', encoding="utf-8") as f:
+    f.write(company_name.split(':')[-1])
+f.close()
+
+
+# import io
+# import re
+# import pdfplumber
+# import pandas as pd
+
+# file_path = 'Sample_Hoa_don.pdf'
+
+# with pdfplumber.open(file_path) as pdf:
+#     page = pdf.pages[0]
+#     text = page.extract_text()
+
+# print(text)
+# # print(type(text))
+
+# # In vao text
+# with io.open('Hoa_don_text_ver_1.txt', 'w', encoding="utf-8") as f:
+#     f.write(text)
+# f.close()
--- sign.py
+++ sign.py
@@ -1,77 +1,3 @@
-# import hashlib
-# from types import CodeType
-# from typing import MutableSequence
-# import rsa
-
-# CA = {}
-
-# def rsaEncrypt(str,privk):
-#     print(str)
-#     content = str.encode('utf-8')
-#     crypto = rsa.sign(content,privk,'SHA-1')
-#     print('here')
-#     print(crypto)
-#     return crypto
-
-
-# def rsaDecrypt(mess, signa, pubk):
-
-#     try:
-#         rsa.verify(mess.encode(),signa,pubk)
-#     except rsa.VerificationError:
-#         result = False
-#     else:
-#         result = True
-#     return result
-
-# def applyKey(name,pubk):
-
-#     CA[name] = pubk
-
-# class User:
-#     def __init__(self,name):
-#         self.name = name
-#         self.crypto = ''
-
-#         (self.pubkey,self.privkey) = rsa.newkeys(512)
-#         print(type(self.pubkey))
-
-#         applyKey(self.name,self.pubkey)
-
-#     def sign(self,str):
-#         self.crypto = rsaEncrypt(hashlib.md5((str + self.name).encode()).hexdigest(),self.privkey)
-#         print(self.crypto.decode('utf-8'))
-#         return self.crypto.decode('unicode_escape')
-
-#     def check(self,str,user,crypto):
-#         crypto=bytes(crypto,'latin-1')
-#         if user.name not in CA:
-#             print("khong ton tai")
-#         else:
-#             pubk = CA[user.name]
-#             if rsaDecrypt(hashlib.md5((str+user.name).encode()).hexdigest(),crypto,pubk):
-#                 print("xac minh thanh cong")
-#             else:
-#                 print("khong xac minh")
-
-# message = b'test123'
-# user1 = User('user1')
-# demo = user1.sign(message)
-# print(type(demo))
-# print(demo)
-
-# user2 = User('user2')
-# user2.check(message, user1, demo)
-# user3 = User('user3')
-# print("{0} {1} ".format(user1.name,message))
-# miwen = user1.sign(message)
-# print(miwen)
-# print(type(miwen))
-# a=miwen.decode('unicode_escape')
-# b=bytes(a,'latin-1')
-
-# user2.check(message,user1,miwen)
-
 from json import encoder
 from tkinter.constants import S
 from Crypto.PublicKey import RSA
@@ -81,12 +7,16 @@ from Crypto.Hash import SHA256
 import binascii
 
 # Generate 1024-bit RSA key pair (private + public key)
+
+
 def generateKey():
-    priKey = RSA.generate(bits=1024) #private key
+    priKey = RSA.generate(bits=1024)  # private key
     pubKey = priKey.publickey()
     return priKey, pubKey
 
 # Sign the message using the PKCS#1 v1.5 signature scheme (RSASP1)
+
+
 def sign_msg(msg, sk):
     hash = SHA256.new(msg)
     signer = PKCS115_SigScheme(sk)
@@ -94,6 +24,8 @@ def sign_msg(msg, sk):
     return signature
 
 # Verify valid PKCS#1 v1.5 signature (RSAVP1)
+
+
 def verify_msg(message, pubkey, sig):
     hash = SHA256.new(message)
     verifier = PKCS115_SigScheme(pubkey)
@@ -104,7 +36,6 @@ def verify_msg(message, pubkey, sig):
     except:
         print("Signature is invalid.")
         return False
-
 
 
 #sk, pk = generateKey()
